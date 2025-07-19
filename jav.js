6c1977ca37d5e1e8c5b4078d9336021590a9c73d
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-07-19 16:48:25 +0700

    added text file to try and discuss potential bugs

--- jav.js
+++ jav.js
@@ -1,6 +1,8 @@
 let result=false
+let secOper=false
 function calc(a){
     result=true
+    secOper=false
     if (a.includes("+")){
         a=a.split("+")
         return parseInt(a[0])+parseInt(a[1])
@@ -25,21 +27,18 @@ for (let i =0; i<9;i++){
         if (result){
             display.textContent=""
             result=false
-    }
+        }
         display.append(buttons[i].textContent)
     })
 }
 for (let i =0; i<4;i++){
     operations[i].addEventListener("click", () =>{
-        if (result){
-            display.textContent=""
-            result=false
-    }
         display.append(operations[i].textContent)
+        secOper=true
     })
 }
 operations[4].addEventListener("click",() =>{
-    display.textContent=calc(display.textContent)
+        display.textContent=calc(display.textContent)
 })
 buttons[9].addEventListener("click",() =>{
     display.append(buttons[9].textContent)
