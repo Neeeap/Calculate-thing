80e3ddb2728132243780852bd029d2518d960ede
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-07-19 16:23:20 +0700

    made calculator actually functional

--- jav.js
+++ jav.js
@@ -1,4 +1,6 @@
+let result=false
 function calc(a){
+    result=true
     if (a.includes("+")){
         a=a.split("+")
         return parseInt(a[0])+parseInt(a[1])
@@ -15,12 +17,30 @@ function calc(a){
 }
 let numbers=document.querySelector("#nums")
 let buttons=numbers.querySelectorAll("button")
+let ope=document.querySelector("#operators")
+let operations=ope.querySelectorAll("button")
 let display=document.querySelector("p")
 for (let i =0; i<9;i++){
     buttons[i].addEventListener("click", () =>{
+        if (result){
+            display.textContent=""
+            result=false
+    }
         display.append(buttons[i].textContent)
     })
 }
+for (let i =0; i<4;i++){
+    operations[i].addEventListener("click", () =>{
+        if (result){
+            display.textContent=""
+            result=false
+    }
+        display.append(operations[i].textContent)
+    })
+}
+operations[4].addEventListener("click",() =>{
+    display.textContent=calc(display.textContent)
+})
 buttons[9].addEventListener("click",() =>{
     display.append(buttons[9].textContent)
 })
